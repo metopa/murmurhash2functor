d3aaa80c9a02e37029f11aa2be8e6f8c3fc1573b
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-17 01:05:21 +0100

    Project files initial commit

---
+++ example/main.cpp
@@ -0,0 +1,12 @@
+/** @file main.cpp
+ *  @brief
+ *
+ *  @author Viacheslav Kroilov (metopa) <slavakroilov@gmail.com>
+ */
+ 
+#include "MurmurHash2.h"
+
+int main() {
+	MurmurHash2(nullptr, 0, 0);
+	return 0;
+}
