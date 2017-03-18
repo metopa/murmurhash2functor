8ec0381fb342c8a3938ee0d89051c908419bbe51
Viacheslav Kroilov <slavakroilov@gmail.com>
2017-03-18 13:56:11 +0100

    Add main include file

---
+++ include/murmurhash2/all.h
@@ -0,0 +1,13 @@
+/** @file all.h
+ *  @brief
+ *
+ *  @author Viacheslav Kroilov (metopa) <slavakroilov@gmail.com>
+ */
+
+#ifndef MURMURHASH2FUNCTOR_ALL_H
+#define MURMURHASH2FUNCTOR_ALL_H
+
+#include "functor.h"
+#include "stl_specializations.h"
+
+#endif //MURMURHASH2FUNCTOR_ALL_H
